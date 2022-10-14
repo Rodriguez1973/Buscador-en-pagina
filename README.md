1df5c682e29e5937d0bb3a84f98c6342e9c4858c
Rodriguez1973 <j062667@gmail.com>
2022-10-14 22:25:14 +0200

    Fin de la depuración.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Buscador en página
 Crear una utilidad en la página que busque en el texto de los elementos <div>. Escribir la palabra a buscar y marcarla en el texto en negrita y rojo.
+
+Notas:
+La aplicación es "case sensitive" por lo que tiene importancia escribir un carácter en mayúsculas o minúsculas.
+En el algoritmo utilizado se recoge el texto de cada una de las etiquetas div contenidas en el <div id="texto"> en un array. A continuación se recorre este array buscando la cadena a buscar, marcando con una etiqueta "<rojonegrita>" antes de la cadena y "</rojonegrita> tras ella. Mediante la hoja de estilos .css se establecen las propiedades dentro de esta.
+Al buscar una nueva cadena se eliminan las etiquetas anteriormente mencionadas para comenzar de nuevo con la busqueda y su marcado.
--- js/javascript.js
+++ js/javascript.js
@@ -2,32 +2,36 @@
  * Fecha: 13/10/2022
  */
 
-const inicioMarcado="<rojonegrita>"; //Inicio del marcado para el tratamiento CSS.
-const finMarcado="</rojonegrita>"; //Fin del marcado para el tratamiento CSS.
+const inicioMarcado="<rojonegrita>"; //Etiqueta de inicio del marcado para el tratamiento CSS.
+const finMarcado="</rojonegrita>"; //Etiqueta de fin del marcado para el tratamiento CSS.
 let contadorPalabras=0; //Contador de palabras.
 
 /*Define la variable que hace referencia al <input id="notificaciones">*/
 let notificaciones=document.getElementById("notificaciones");
 
-/*Define el escuchador del evento click en <svg id="icono_buscar">*/
+/*Define la variable que hace referencia al <svg id="icono_buscar">*/
 let buscar=document.getElementById("icono_buscar");
+/*Define el escuchador del evento click en <svg id="icono_buscar">*/
 buscar.addEventListener("click", buscarPalabra, false);
 
-/*Define el escuchador del evento focus en <input id="input_buscar>"*/
+/*Define la variable que hace referencia al <input id="input_buscar>"*/
 let entradaTexto=document.getElementById("input_buscar");
+/*Define el escuchador del evento focus en <input id="input_buscar>"*/
 entradaTexto.addEventListener("focus", seleccionarContenido, false);
-
-/*Define el escuchador del evento click en <a id="cerrar_navegador">*/
-let cerrar=document.getElementById("cerrar_navegador");
-cerrar.addEventListener("click", cerrarNavegador, false);
-
 /*Define el escuchador del evento keydown en <input id="input_buscar>"*/
 entradaTexto.addEventListener("keydown", comprobarEnter, false);
 
+/*Define la variable que hace referencia al <a id="cerrar_navegador">*/
+let cerrar=document.getElementById("cerrar_navegador");
+/*Define el escuchador del evento click en <a id="cerrar_navegador">*/
+cerrar.addEventListener("click", cerrarNavegador, false);
+
+
 /*Establece el foco en el <input id="input_buscar> al cargar la página*/
 window.onload=function(){
-    buscar.select();
+    entradaTexto.focus();
 }
+
 
 /*Función que comprueba que se ha pulsado la tecla Enter en el <input id="input_buscar>" con lo que lanza el proceso de busqueda de forma similar a hacer click sobre el icono de la lupa*/
 function comprobarEnter(evento){
@@ -36,34 +40,44 @@ function comprobarEnter(evento){
     }
 }
 
+
 /*Función que realiza el cierre del navegador*/
 function cerrarNavegador(){
     window.close();
 }
+
 
 /*Función que selecciona el contenido de <input id="input_buscar> al tomar el foco*/
 function seleccionarContenido(){
     entradaTexto.select();
 }
 
+
 /*Función que realiza la busqueda de la cadena de caracteres dentro del texto*/
 function buscarPalabra(){
     //Palabra a buscar.
     let palabraBuscar=document.getElementById("input_buscar").value.trim();
-       
+    
+    //Si la cadena a buscar no está vacia.
     if(palabraBuscar.length>0){
         contadorPalabras=0;
-        var textos=document.querySelectorAll("#texto div");
+        //Array con el contenido de cada unos de los div dentro del contenedor <div id="texto">
+        let textos=document.querySelectorAll("#texto div");
+        //Bucle que recorre cada elemento del array.
         textos.forEach(elemento => {
             let texto=elemento.innerHTML.trim();
+            //Si el texto incluye la cadena de inicioMarcado, borra las etiquetas de marcado.
             if(texto.includes(inicioMarcado)){
                 texto=borrar_etiquetas(texto)
             }
+            //Si el texto incluye la cadena a buscar, realiza su marcado.
             if(texto.includes(palabraBuscar)){
                 texto = buscar_Y_marcar(texto, palabraBuscar)
             }
+            //Asigna el texto tras su procesado, al elemento div tratado.
             elemento.innerHTML=texto;
         });
+        //¿Existe la cadena de caracteres buscada?. Mostrar notificaciones.
         if(contadorPalabras>0) {
             notificaciones.value=`La cadenas de caracteres buscada existe ${contadorPalabras} veces en el texto.`;
         }else{
@@ -72,22 +86,27 @@ function buscarPalabra(){
     }
 }
 
+
 /*Función que realiza la busqueda del texto y realiza el marcado para tramiento CSS*/
 function buscar_Y_marcar(texto, palabraBuscar){
     let indiceInicio=0;
+    //Mientras la cadena a buscar existe en la porción de texto tratada.
     while(texto.indexOf(palabraBuscar,indiceInicio)!=-1){
         let indiceFinal=texto.indexOf(palabraBuscar,indiceInicio);
+        //Realiza el marcado.
         texto=texto.substring(0,indiceFinal)+inicioMarcado+palabraBuscar+finMarcado+texto.substring(indiceFinal+palabraBuscar.length);
+        //Recalcula indiceInicio empezando tras el último marcado.
         indiceInicio=indiceFinal+inicioMarcado.length+finMarcado.length+palabraBuscar.length;
         contadorPalabras++;
     }
-    console.log(texto);
     return texto;
 }
 
 /*Función que realiza el borrado de las etiquetas de marcado CSS*/
 function borrar_etiquetas(texto){
+    //Elimina todas las cadenas inicioMarcado del texto.
     texto=texto.replaceAll(inicioMarcado,"");
+    //Elimina todas las cadenas finMarcado del texto.
     texto=texto.replaceAll(finMarcado,"");
     return texto;
 }
