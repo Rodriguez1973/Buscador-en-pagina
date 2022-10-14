335b2150292cf78ebb68c09ed83d5b101d7fcfa7
Rodriguez <j062667@gmail.com>
2022-10-14 10:50:44 +0200

    Se añade contador de cadenas encontradas.

--- js/javascript.js
+++ js/javascript.js
@@ -6,21 +6,28 @@ const inicioMarcado="<rojonegrita>"; //Inicio del marcado para el tratamiento CS
 const finMarcado="</rojonegrita>"; //Fin del marcado para el tratamiento CSS.
 let contadorPalabras=0; //Contador de palabras.
 
+/*Define la variable que hace referencia al <input id="notificaciones">*/
+let notificaciones=document.getElementById("notificaciones");
+
 /*Define el escuchador del evento click en <svg id="icono_buscar">*/
-buscar=document.getElementById("icono_buscar");
+let buscar=document.getElementById("icono_buscar");
 buscar.addEventListener("click", buscarPalabra, false);
 
 /*Define el escuchador del evento focus en <input id="input_buscar>"*/
-entradaTexto=document.getElementById("input_buscar");
+let entradaTexto=document.getElementById("input_buscar");
 entradaTexto.addEventListener("focus", seleccionarContenido, false);
 
 /*Define el escuchador del evento click en <a id="cerrar_navegador">*/
-cerrar=document.getElementById("cerrar_navegador");
+let cerrar=document.getElementById("cerrar_navegador");
 cerrar.addEventListener("click", cerrarNavegador, false);
 
 /*Define el escuchador del evento keydown en <input id="input_buscar>"*/
-entradaTexto=document.getElementById("input_buscar");
 entradaTexto.addEventListener("keydown", comprobarEnter, false);
+
+/*Establece el foco en el <input id="input_buscar> al cargar la página*/
+window.onload=function(){
+    buscar.select();
+}
 
 /*Función que comprueba que se ha pulsado la tecla Enter en el <input id="input_buscar>" con lo que lanza el proceso de busqueda de forma similar a hacer click sobre el icono de la lupa*/
 function comprobarEnter(evento){
@@ -45,6 +52,7 @@ function buscarPalabra(){
     let palabraBuscar=document.getElementById("input_buscar").value.trim();
        
     if(palabraBuscar.length>0){
+        contadorPalabras=0;
         var textos=document.querySelectorAll("#texto div");
         textos.forEach(elemento => {
             let texto=elemento.innerHTML.trim();
@@ -57,9 +65,9 @@ function buscarPalabra(){
             elemento.innerHTML=texto;
         });
         if(contadorPalabras>0) {
-            
+            notificaciones.value=`La cadenas de caracteres buscada existe ${contadorPalabras} veces en el texto.`;
         }else{
-
+            notificaciones.value="La cadena de caracteres buscada no existe en el texto.";
         }
     }
 }
