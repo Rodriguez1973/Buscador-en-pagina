c98a1c89eea031bce1882fee2b8eb988cc72cbac
Rodriguez1973 <j062667@gmail.com>
2022-10-14 04:59:54 +0200

    Depurando borrado de etiquetas.

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,9 @@
  * Fecha: 13/10/2022
  */
 
+const inicioMarcado="<rojoNegrita>";
+const finMarcado="</rojoNegrita>";
+
 /*Define el escuchador del evento click en <svg id="icono_buscar">*/
 buscar=document.getElementById("icono_buscar");
 buscar.addEventListener("click", buscarPalabra, false);
@@ -23,6 +26,9 @@ function buscarPalabra(){
         var textos=document.querySelectorAll("#texto div");
         textos.forEach(elemento => {
             let texto=elemento.innerHTML.trim();
+            if(texto.includes(inicioMarcado)){
+                texto=borrar_etiquetas(texto)
+            }
             if(texto.includes(palabraBuscar)){
                 texto = buscar_Y_marcar(texto, palabraBuscar)
             }
@@ -35,13 +41,15 @@ function buscar_Y_marcar(texto, palabraBuscar){
     let indiceInicio=0;
     while(texto.indexOf(palabraBuscar,indiceInicio)!=-1){
         let indiceFinal=texto.indexOf(palabraBuscar,indiceInicio);
-        texto=texto.substring(0,indiceFinal)+"<rojoNegrita>"+palabraBuscar+"</rojoNegrita>"+texto.substring(indiceFinal+palabraBuscar.length);
-        indiceInicio=indiceFinal+"<rojoNegita>".length+"</rojoNegrita>".length+palabraBuscar.length;
+        texto=texto.substring(0,indiceFinal)+inicioMarcado+palabraBuscar+finMarcado+texto.substring(indiceFinal+palabraBuscar.length);
+        indiceInicio=indiceFinal+inicioMarcado.length+finMarcado.length+palabraBuscar.length;
     }
     console.log(texto);
     return texto;
 }
 
-function borrar_etiquetas(texto,etiquetaInicio,etiquetaFin){
-    let
+function borrar_etiquetas(texto){
+    texto=texto.replaceAll(inicioMarcado,"");
+    texto=texto.replaceAll(finMarcado,"");
+    return texto;
 }
